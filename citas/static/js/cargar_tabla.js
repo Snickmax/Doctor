79a74b0c966774cab6b9f9ac96b741dd07e4170f
Snickmax <victor.2002.espinoza@gmail.com>
2024-06-14 00:45:36 -0400

    datatable active

--- citas/static/js/cargar_tabla.js
+++ citas/static/js/cargar_tabla.js
@@ -1,13 +1,13 @@
 $(document).ready(function () {
     $("#table-user").DataTable({
         processing: true,
-        "dom": 'rtip',
+        dom: 'rtip',
         orderable: true,
         language: {
             url: "//cdn.datatables.net/plug-ins/1.13.7/i18n/es-CL.json",
         },
-        columnDefs: [{ orderable: false, targets: 6 }],
-        order: [[5, "asc"], [3, "asc"], [2, "asc"]],
+        columnDefs: [{ orderable: false, targets: 5 }],
+        order: [[0, "asc"]],
     });
     $('#search-input').on('keyup', function () {
         $('#table-user').DataTable().search(this.value).draw();
